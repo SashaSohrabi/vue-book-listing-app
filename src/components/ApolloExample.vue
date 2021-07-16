d232854dd0804b8818e32094d6ae1028eb6c1a97
Sasha Sohrabi <sasha.sohrabi@valtech.com>
2021-07-17 02:08:54 +0300

    created ApolloExample file

--- src/components/ApolloExample.vue
+++ src/components/ApolloExample.vue
@@ -1,14 +1,76 @@
 <template>
-  <h1>{{ books }}</h1>
+  <div class="container">
+    <div class="col-md-12">
+      <h1>Book listing APP With Vue.js Apollo and GraphQL</h1>
+      <form>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Title"
+            v-model="title"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Author"
+            v-model="author"
+          />
+        </div>
+        <div class="form-group">
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="Descrioption"
+            v-model="description"
+          >
+          </textarea>
+        </div>
+        <button
+          type="button"
+          class="btn btn-secondary 
+              btn-lg btn-block"
+          @click.prevent="createBook"
+        >
+          Create book
+        </button>
+      </form>
+    </div>
+    <div class="row">
+      <div class="container mt-4">
+        <div v-for="book in books" :key="book.id">
+          <div class="col-md-12">
+            <div class="">
+              <div class="card">
+                <div class="card-body">
+                  Title: {{ book.title }}
+                  <hr />
+                  Author: {{ book.author }}, Descroption: {{ book.description }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import gql from 'graphql-tag';
 export default {
-  name: 'mainApp',
   data() {
     return {
       books: '',
+      title: '',
+      author: '',
+      description: '',
     };
   },
   apollo: {
@@ -22,6 +84,46 @@ export default {
           }
         }
       `,
+    },
+  },
+
+  methods: {
+    createBook() {
+      console.log(this.title, this.authour, this.description);
+      if (this.title != '' && this.author != '' && this.description != '') {
+        this.$apollo
+          .mutate({
+            mutation: gql`
+              mutation createBook(
+                $title: String!
+                $author: String!
+                $description: String!
+              ) {
+                createBook(
+                  title: $title
+                  author: $author
+                  description: $description
+                ) {
+                  title
+                  author
+                  description
+                }
+              }
+            `,
+            variables: {
+              title: this.title,
+              author: this.author,
+              description: this.description,
+            },
+          })
+          .then(response => {
+            this.books = response.data.createBook;
+            console.log(this.books);
+            // location.reload();
+          });
+      } else {
+        alert('Please fill all the fields');
+      }
     },
   },
 };
